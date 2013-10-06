a457e47571ca3ee17ee099b47db040c225e79660
Geoff Crittenden <geoffcritt@hotmail.com>
2013-10-06 11:00:35 -0500

    error with create method

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Blog::Application.config.secret_token = '12b0d67c7e453a5f9902b3180a311df3a9a8693fc8db7805fa072cf7626ddb8197a17ac8230cdb4e61765f6ee232d1dca230c398e2bdf4e82f06d63940bbe5fc'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'ed2d44655596f33c39ab19803bd30da559d1cf1dabe0cc4ec7080afd85202cf14fc587aaec5e84fcbef78179dd5b2c70a376c2ef41d763b8b2d19b55db4ff27a'
